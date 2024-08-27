084680f3507ac2f77c4a4533a3622724f560cc6c
murhaf2211 <orfali.murhaf@web.de>
2024-08-27 11:03:11 +0200

    new project ,task in interfaces

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        MusicPlayer player = new MusicPlayer();
+        player.play();
+        VideoPlayer videoPlayer = new VideoPlayer();
+        videoPlayer.play();
+
+        MediaController mediaController = new MediaController();
+        mediaController.playMedia(player);
+        mediaController.playMedia(videoPlayer);
+    }
+}
